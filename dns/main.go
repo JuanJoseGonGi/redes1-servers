3de7820ffa3c6c2b2f602b7f2699a8e06b8c8485
Juan José González Giraldo <juanjose.eng@gmail.com>
2022-12-08 20:57:04 -0500

    Change DNS to handle the records on a file

--- dns/main.go
+++ dns/main.go
@@ -8,9 +8,7 @@ import (
 	"github.com/miekg/dns"
 )
 
-var records = map[string]string{
-	"redes.cucho.": "192.168.0.4",
-}
+var records = map[string]string{}
 
 func parseQuery(msg *dns.Msg) {
 	for _, question := range msg.Question {
@@ -42,6 +40,41 @@ func handleDNSRequest(writer dns.ResponseWriter, r *dns.Msg) {
 	}
 }
 
+func parseRecordsFile(filename string) error {
+	file, err := os.Open(filename) // #nosec
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Printf("Failed to close file: %s\n", err.Error())
+		}
+	}()
+
+	var line string
+
+	for {
+		_, err = fmt.Fscanf(file, "%s", &line)
+		if err != nil {
+			return fmt.Errorf("failed to read line: %w", err)
+		}
+
+		var name, ip string
+
+		_, err = fmt.Sscanf(line, "%s %s", &name, &ip)
+		if err != nil {
+			log.Printf("Failed to parse line: %s\n", line)
+			continue
+		}
+
+		records[name] = ip
+
+		log.Printf("Added record: %s -> %s", name, ip)
+	}
+}
+
 func main() {
 	// attach request handler func
 	dns.HandleFunc("cucho.", handleDNSRequest)
@@ -51,18 +84,28 @@ func main() {
 		port = os.Args[1]
 	}
 
+	recordsFile := "records.txt"
+	if len(os.Args) > 2 && os.Args[2] != "" {
+		recordsFile = os.Args[2]
+	}
+
+	err := parseRecordsFile(recordsFile)
+	if err != nil {
+		log.Fatalf("Failed to parse records file: %s\n", err.Error())
+	}
+
 	server := &dns.Server{Addr: ":" + port, Net: "udp"}
 
 	log.Printf("Starting at %s\n", port)
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Failed to start server: %s\n ", err.Error())
+	if err = server.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to start server: %s\n", err.Error())
 	}
 
 	defer func() {
 		err := server.Shutdown()
 		if err != nil {
-			log.Fatalf("Failed to shutdown server: %s\n ", err.Error())
+			log.Fatalf("Failed to shutdown server: %s\n", err.Error())
 		}
 	}()
 }
